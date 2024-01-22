05bea5b1851316e6968d7324c9dc237a9791b401
naiomi <Naiomi.Naidoo@icloud.com>
2024-01-22 21:13:59 +0100

    Implementing debit service

--- src/main/java/com/naiomi/paymentservice/initiation/directdebit/DirectDebitServiceImpl.java
+++ src/main/java/com/naiomi/paymentservice/initiation/directdebit/DirectDebitServiceImpl.java
@@ -1,5 +1,10 @@
 package com.naiomi.paymentservice.initiation.directdebit;
 
+import com.naiomi.paymentservice.account.AccountDto;
+import com.naiomi.paymentservice.account.AccountServiceClient;
+import com.naiomi.paymentservice.account.BalanceDto;
+import com.naiomi.paymentservice.exceptions.InsufficientFundsException;
+import com.naiomi.paymentservice.initiation.data.PaymentStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +15,11 @@ public class DirectDebitServiceImpl implements DirectDebitService{
 
     private final DirectDebitRepository directDebitRepository;
 
-    public DirectDebitServiceImpl(DirectDebitRepository directDebitRepository) {
+    private final AccountServiceClient accountServiceClient;
+
+    public DirectDebitServiceImpl(DirectDebitRepository directDebitRepository, AccountServiceClient accountServiceClient) {
         this.directDebitRepository = directDebitRepository;
+        this.accountServiceClient = accountServiceClient;
     }
 
     @Override
@@ -26,7 +34,33 @@ public class DirectDebitServiceImpl implements DirectDebitService{
 
     @Override
     public DirectDebit createDirectDebit(DirectDebit directDebit) {
+        AccountDto debtorAccount = getAccount(directDebit.getDebtorAccountId());
+        AccountDto creditorAccount = getAccount(directDebit.getCreditorAccountId());
+        boolean balanceIsValid = doesAccountHaveEnoughFunds(debtorAccount, directDebit.getAmount());
+        if (!balanceIsValid) {
+            directDebit.setStatus(PaymentStatus.INSUFFICIENT_FUNDS);
+            directDebitRepository.save(directDebit);
+            throw new InsufficientFundsException("Insufficient funds to complete payment");
+        }
+        calculateBalance(debtorAccount, creditorAccount, directDebit.getAmount());
+
         return directDebitRepository.save(directDebit);
+    }
+
+    public AccountDto getAccount(String id) {
+        return accountServiceClient.getAccount(id);
+    }
+
+
+    public void calculateBalance(AccountDto debtorAccount, AccountDto creditorAccount, double amount) {
+        BalanceDto debtorBalance = new BalanceDto(debtorAccount.getBalance() - amount);
+        accountServiceClient.updateAccountBalance(debtorAccount.getId(), debtorBalance);
+        BalanceDto creditorBalance = new BalanceDto(creditorAccount.getBalance() + amount);
+        accountServiceClient.updateAccountBalance(creditorAccount.accountId(), creditorBalance);
+    }
+
+    public boolean doesAccountHaveEnoughFunds(AccountDto account, double amount) {
+        return account.getBalance() >= amount;
     }
 
     @Override
@@ -38,4 +72,6 @@ public class DirectDebitServiceImpl implements DirectDebitService{
     public void delete(String id) {
         directDebitRepository.deleteById(id);
     }
+
+
 }

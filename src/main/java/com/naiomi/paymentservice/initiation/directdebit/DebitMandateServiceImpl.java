3a0ff72e7e320819457238a116299b9f05b3c8c9
naiomi <Naiomi.Naidoo@icloud.com>
2024-01-24 22:49:36 +0100

    Implementing mandate scheduling

--- src/main/java/com/naiomi/paymentservice/initiation/directdebit/DebitMandateServiceImpl.java
+++ src/main/java/com/naiomi/paymentservice/initiation/directdebit/DebitMandateServiceImpl.java
@@ -2,8 +2,19 @@ package com.naiomi.paymentservice.initiation.directdebit;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DebitMandateServiceImpl implements DebitMandateService{
 
+    private final DebitMandateRepository debitMandateRepository;
 
+    public DebitMandateServiceImpl(DebitMandateRepository debitMandateRepository) {
+        this.debitMandateRepository = debitMandateRepository;
+    }
+
+    @Override
+    public List<DebitMandate> findAllByStatus(String status) {
+        return debitMandateRepository.findAllByStatus(status);
+    }
 }
